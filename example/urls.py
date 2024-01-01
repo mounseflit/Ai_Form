a71beb7a020780ea542566b2ad1951ad905c7dcd
Mounsef Litniti <93012372+mounseflit@users.noreply.github.com>
2024-01-01 06:12:13 +0100

    Waaaaaaaaaaaaaaaaa3

--- example/urls.py
+++ example/urls.py
@@ -9,5 +9,5 @@ from example.views import admin
 urlpatterns = [
     path('save_transcription/', save_audio, name='save_audio1'),
     path('', index, name='save_audio2'),
-    path('admin_lydec', admin, name='admin_lydec'),
+    path('prompt_admin', admin, name='prompt_admin'),
 ]
